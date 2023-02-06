04a898279e059e4a3d6ba9e51ebd6769d524db30
Justin Duong <jduong@umich.edu>
2023-02-05 23:57:05 -0500

    First commit.

---
+++ assets/js/headerMenu.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const imgHamburger = document.getElementById('img-hamburger');
+  const imgClose = document.getElementById('img-close');
+  const listHeaderMenu = document.querySelector('.list-nav-menu');
+  const divHeaderBg = document.getElementById('div-bg-header-open');
+
+  imgHamburger.addEventListener('click', () => {
+    imgHamburger.classList.add('hide');
+    imgClose.classList.remove('hide');
+    listHeaderMenu.classList.remove('hide');
+    divHeaderBg.classList.remove('hide');
+  });
+
+  imgClose.addEventListener('click', () => {
+    imgHamburger.classList.remove('hide');
+    imgClose.classList.add('hide');
+    listHeaderMenu.classList.add('hide');
+    divHeaderBg.classList.add('hide');
+  });
+});
