73a898004f5864c3970bc2397e3e90d77f25f78a
Justin Duong <jduong@umich.edu>
2023-02-23 19:47:35 -0500

    More work on logic for div form.

--- assets/js/listUserChoicesHeader.js
+++ assets/js/listUserChoicesHeader.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const iconArrows = document.querySelectorAll('.div-input-header > button');
-
+  const listItemsFormSteps = document.getElementsByClassName('list-item-form-step');
   for (let i = 0; i < iconArrows.length; i++) {
     const iconArrow = iconArrows[i];
 
@@ -8,14 +8,55 @@ document.addEventListener('DOMContentLoaded', () => {
       const divInputHeader = e.currentTarget.parentElement;
       const divForm = divInputHeader.parentElement;
 
+      // if inputHeader is open, then we remove open on the divInputHeader parent element
+      // This will rotate the svg
+      // Then we hide the divForm
       if (divInputHeader.classList.contains('open')) {
         divInputHeader.classList.remove('open');
 
         divForm.children[1].classList.add('hide');
+
+
+        if (window.innerWidth >= 1200 && i != 4) {
+          // When an icon arrow is opened, we want to close the other icon arrows and their respective lists.
+          // We then want to highlight the corresponding header list item, and dehighlight the other ones.
+          listItemsFormSteps[i].classList.remove('selected');
+          const currentDivInputHeader = iconArrows[i + 1].parentElement;
+          const currentDivForm = currentDivInputHeader.parentElement;
+
+          currentDivInputHeader.classList.add('open');
+          currentDivForm.children[1].classList.remove('hide');
+          listItemsFormSteps[i + 1].classList.add('selected');
+        }
       } else {
+        // InputHeader is closed, we use the parent element to set the class to open and rotate the svg
+        // Remove the hide on the sibling list element
         divInputHeader.classList.add('open');
 
         divForm.children[1].classList.remove('hide');
+
+        // If the window is in desktop view (> 1200px)
+        // When this icon arrow is opened, we want to close the other icon arrows
+        // If there is an open icon arrow, we want to hide its respective list element
+        // we need to highlight/dehighlight the divFormHeader Items.
+        if (window.innerWidth >= 1200) {
+          // When an icon arrow is opened, we want to close the other icon arrows and their respective lists.
+          // We then want to highlight the corresponding header list item, and dehighlight the other ones.
+          for (let j = 0; j < iconArrows.length; j++) {
+            if (i === j) {
+              listItemsFormSteps[i].classList.add('selected');
+              continue;
+            } else {
+              const currentDivInputHeader = iconArrows[j].parentElement;
+              const currentDivForm = currentDivInputHeader.parentElement;
+
+              currentDivInputHeader.classList.remove('open');
+              currentDivForm.children[1].classList.add('hide');
+              listItemsFormSteps[j].classList.remove('selected');
+
+            }
+          } 
+        }
       }
     });
   }
--- assets/js/resize.js
+++ assets/js/resize.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const spanCheckoutCalculatedPrice = document.getElementById('span-checkout-calculated-price');
   const divHeroDescription = document.querySelector('.div-hero > div > div');
   const divFormHeaderSteps = document.getElementById('div-form-header-steps');
+
+  const listItemFormSteps = document.getElementsByClassName('list-item-form-step');
+  const divFormItems = document.getElementsByClassName('div-form-item');
   // For plan.html, on resize we are going to move the checkout calculated price outside of the button.
   window.addEventListener('resize', () => {
     if (window.innerWidth >= 768 && divOrderButtonConfirmation.children.length === 1) {
@@ -16,9 +19,26 @@ document.addEventListener('DOMContentLoaded', () => {
       btnCheckoutConfirm.append(spanCheckoutCalculatedPrice);
     }
 
-    if (window.innerWidth >= 1400) {
+    if (window.innerWidth >= 1200) {
       divHeroDescription.innerText = 'Coffee the way you wanted it to be. For coffee delivered tomorrow, or next week. For whatever brew method you use. For choice, for convenience, for quality.';
       divFormHeaderSteps.classList.remove('hide');
+
+      const listItemFormStepsSelected = document.querySelectorAll('.list-item-form-step.selected');
+      // if we resize to 1200px, desktop view, then hide the bottom 4 lists and close their lists.
+      if (listItemFormStepsSelected.length != 1) {
+        listItemFormSteps[0].classList.add('selected');
+
+        divFormItems[1].children[0].classList.remove('open');
+        divFormItems[1].children[1].classList.add('hide');
+        divFormItems[2].children[0].classList.remove('open');
+        divFormItems[2].children[1].classList.add('hide');
+        divFormItems[3].children[0].classList.remove('open');
+        divFormItems[3].children[1].classList.add('hide');
+        divFormItems[4].children[0].classList.remove('open');
+        divFormItems[4].children[1].classList.add('hide');
+      } else {
+        return;
+      }
     } else {
       divHeroDescription.innerText = 'Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.';
       divFormHeaderSteps.classList.add('hide');
@@ -35,9 +55,26 @@ document.addEventListener('DOMContentLoaded', () => {
     btnCheckoutConfirm.append(spanCheckoutCalculatedPrice);
   }
 
-  if (window.innerWidth >= 1400) {
+  if (window.innerWidth >= 1200) {
     divHeroDescription.innerText = 'Coffee the way you wanted it to be. For coffee delivered tomorrow, or next week. For whatever brew method you use. For choice, for convenience, for quality.';
     divFormHeaderSteps.classList.remove('hide');
+
+    const listItemFormStepsSelected = document.querySelectorAll('.list-item-form-step.selected');
+    // if we resize to 1200px, desktop view, then hide the bottom 4 lists and close their lists.
+    if (listItemFormStepsSelected.length != 1) {
+      listItemFormSteps[0].classList.add('selected');
+      
+      divFormItems[1].children[0].classList.remove('open');
+      divFormItems[1].children[1].classList.add('hide');
+      divFormItems[2].children[0].classList.remove('open');
+      divFormItems[2].children[1].classList.add('hide');
+      divFormItems[3].children[0].classList.remove('open');
+      divFormItems[3].children[1].classList.add('hide');
+      divFormItems[4].children[0].classList.remove('open');
+      divFormItems[4].children[1].classList.add('hide');
+    } else {
+      return;
+    }
   } else {
     divHeroDescription.innerText = 'Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.';
     divFormHeaderSteps.classList.add('hide');
